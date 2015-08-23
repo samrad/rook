94ce2f8a1a5d03d4dace212621b3ed7926498c37
Sam Nikobonyadrad <snikobonyadrad@ancestry.com>
2015-08-23 13:36:03 -0700

    rendering is put into model

--- js/background.js
+++ js/background.js
@@ -1,41 +1,25 @@
-db.getTabs();
+model.queryTabs();
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         "use strict";
 
-        if (request.action === "init") {
-            sendResponse({tabs: db.get()});
-            //chrome.tabs.query({}, function (tabs) {
-            //    db.set(tabs);
-            //    sendResponse({tabs: db.get()});
-            //});
-
-
-            //// Handle initial load
-            //if (request.action === "get_tabs") {
-            //    console.log('got get_tabs in background');
-            //    var t_promise = getTabs();
-            //    t_promise.then(function(result) {
-            //        console.log('t resolved');
-            //        sendResponse({fuck: result});
-            //    });
-
-            // Handle search
-        } else if (request.action === "search") {
-            sendResponse({tabs: db.get(request.term)});
+        if (request.action === "get_html") {
+            sendResponse({result: model.getHTML(request.term)});
         }
-        return true;
     }
 );
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    db.getTabs();
+    console.log('tabs updated');
+    model.queryTabs();
 });
 
 chrome.tabs.onCreated.addListener(function (tabId, changeInfo, tab) {
-    db.getTabs();
+    console.log('tab created');
+    model.queryTabs();
 });
+
 
 
 
